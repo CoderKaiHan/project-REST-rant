d34e7ed206a0080217d6cc4767e7e696abf620bb
CoderKaiHan <hankaicoder@gmail.com>
2024-02-06 10:52:09 -0500

    Add comments on show view.

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,18 +2,39 @@ const React = require('react');
 const Def = require('../default');
 
 function show (data) {
+  let comments = (
+    <h3 className='inactive'>
+      No comments yet!
+    </h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map ( c => {
+      return (
+        <div className='border'>
+          <h2 className='rant'>
+            {c.rant ? 'Rant!' : 'Rave!'}
+          </h2>
+          <h4>{c.content}</h4>
+          <h3>
+            <strong> - {c.author}</strong>
+          </h3>
+          <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
     return (
         <Def>
           <main>
             <h1>{data.place.name}</h1>
-            <h3>Rating</h3>
+            <h2>Rating</h2>
             <p>Not Rated</p>
             <h3>{data.place.showEstablished()}</h3>
             <h4>Serving {data.place.cuisines}</h4>
             <img src={`${data.place.pic}`} alt={`${data.place.name}`} />
             <h3>Located in {data.place.city}, {data.place.state}</h3>
-            <h3>Comments</h3>
-            <p>No comments yet!</p>
+            <h2>Comments</h2>
+            {comments}
             <a href={`/places/${data.id}/edit`} className='btn btn-warning'>
                 Edit
             </a>
